02bc13efceaa4be7c15fc2534417db1eaac6bf08
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-03-02 22:35:53 +0530

    8

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/pages/routes.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rakesh App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Rakesh Bhardwaj App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-  String name = "Ram dutt";
-  int day = 30;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
-
+        // fontFamily: GoogleFonts.lato().fontFamily,
       ),
- 
-      initialRoute: "/login",
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
       routes: {
-        "/" :(context) => const LoginPage(),
-        MyRoutes.homeRoute:((context) => const HomePage()),
-        MyRoutes.loginRoute:((context) => const LoginPage())
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
+        // MyRoutes.homeRoute
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -47,14 +47,38 @@ class _LoginPageState extends State<LoginPage> {
                     const SizedBox(
                       height: 20,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      child: const Text("Login"),
-                      style: TextButton.styleFrom(
-                          minimumSize: const Size(100, 50)),
+                    InkWell(
+                      onTap: () =>
+                          Navigator.pushNamed(context, MyRoutes.homeRoute),
+                      child: AnimatedContainer(
+                        width: 150,
+                        duration: const Duration(seconds: 1),
+                        height: 50,
+                        alignment: Alignment.center,
+                        // color: Colors.deepPurple,
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            // fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                      
                     )
+
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: const Text("Login"),
+                    //   style: TextButton.styleFrom(
+                    //       minimumSize: const Size(100, 50)),
+                    // )
                   ],
                 ),
               ),
